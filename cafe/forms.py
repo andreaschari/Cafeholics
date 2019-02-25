82b328a3f9534ff22442abff6598aa4c9c6adc7c
Andreas Chari <andreas9810@outlook.com>
2019-02-25 16:20:47 +0000

    added slug field on the Cafe model, created forms for the User, Cafe and Review.

---
+++ cafe/forms.py
@@ -0,0 +1,49 @@
+from django import forms
+from django.contrib.auth.models import User
+from cafe.models import *
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        # include the following fields in the form.
+        fields = ('username', 'first_name', 'last_name' 'email', 'password')
+
+
+class UserProfileForm(forms.ModelForm):
+    is_owner = forms.BooleanField(help_text='Check if you are an owner of a cafe.')
+
+    class Meta:
+        model = UserProfile
+        # include the following fields in the form.
+        fields = ('is_owner',)
+
+
+class CafeForm(forms.ModelForm):
+    # the owner is input by the view
+    owner = forms.CharField(widget=forms.HiddenInput(), required=False)
+    name = forms.CharField(max_length=128, unique=True, required=True)
+    picture = forms.ImageField(upload_to='cafe_images', blank=True)
+    pricepoint = forms.IntegerField(max_length=3, help_text='Enter a price average for your cafe.')
+    slug = models.SlugField(widget=forms.HiddenInput(), required=False, unique=True)
+
+    class Meta:
+        model = Cafe
+        # include the following fields in the form.
+        fields = ('name', 'picture', 'pricepoint')
+
+
+class ReviewForm(forms.ModelForm):
+    price = forms.IntegerField(help_text='Enter a value from 1 out of 5')
+    service = forms.IntegerField(help_text='Enter a value from 1 out of 5')
+    atmosphere = forms.IntegerField(help_text='Enter a value from 1 out of 5')
+    quality = forms.IntegerField(help_text='Enter a value from 1 out of 5')
+    waiting_time = forms.IntegerField(help_text='Enter a value from 1 out of 5')
+    comments = forms.CharField(max_length=500, blank=True)
+
+    class Meta:
+        model = Review
+        # include all fields in the form.
+        fields = '__all__'
--- cafe/models.py
+++ cafe/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.template.defaultfilters import slugify
+
 
 class UserProfile(models.Model):
-    #Links UserProfile to a User model instance.
+    # Links UserProfile to a User model instance.
     user = models.OneToOneField(User,  on_delete=models.CASCADE)
     is_owner = models.BooleanField()
 
@@ -13,24 +15,30 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
+
 class Cafe(models.Model):
     owner = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     name = models.CharField(max_length=20, unique=True)
     picture = models.ImageField(upload_to='cafe_images', blank=True)
-    pricepoint = models.IntegerField(max_length=50)
+    pricepoint = models.IntegerField(validators=[MaxValueValidator(3), MinValueValidator(1)])
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Cafe, self).save(*args, **kwargs)
 
     class Meta:
-        verbose_name_plural = "Cafes"
+        verbose_name_plural = 'Cafes'
 
     def __str__(self):
         return self.name
 
-        
+
 class Review(models.Model):
-    #linking reviews with Cafes
+    # linking reviews with Cafes
     cafe = models.ForeignKey(Cafe, on_delete=models.CASCADE)
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    #criteria to rate the cafe
+    # criteria to rate the cafe
     price = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
     service = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
     atmosphere = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
@@ -39,5 +47,6 @@ class Review(models.Model):
     comments = models.CharField(max_length=500, blank=True)
 
     class Meta:
-        unique_together = ('cafe','user')
+        # joins cafe and user as primary keys
+        unique_together = ('cafe', 'user')
         verbose_name_plural = "Reviews"
