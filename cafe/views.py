b912b9501cc286cf5ea147c4f16080bf06fc0528
Ruchira Ravishankar <2300294r@student.gla.ac.uk>
2019-03-12 13:46:59 +0000

    minor change to imports

--- cafe/views.py
+++ cafe/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from cafe.forms import Cafe, CafeForm, ReviewForm, UserForm, UserProfileForm
+from cafe.forms import Cafe, CafeForm, ReviewForm, UserForm, UserProfileForm, Review
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
